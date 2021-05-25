70985e9cf355b17fbc5c6dd49af08efb4c52dfda
IhorWenher <73756717+IhorWenher@users.noreply.github.com>
2021-05-25 23:49:06 +0300

    Добавил логику, стили, разметку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import './sass/main.scss';
+import countryMarkup from './templates/one-country.hbs';
+import coutryListMarkup from './templates/country-list.hbs';
+import { error } from '@pnotify/core';
+
+
+const refs = {
+    input: document.querySelector('.js-input'),
+    countryList: document.querySelector('.country-list')
+}
+
+function createCountryMarkup(country) {
+    return country.map(countryMarkup).join('');
+}
+
+function createCountryListMarkup(country) {
+    return country.map(coutryListMarkup).join('');
+}
+
+
+
+
+const add = () => {
+    refs.countryList.innerHTML = "";
+
+    fetch(`https://restcountries.eu/rest/v2/name/${refs.input.value}`)
+        .then(responce => {
+            return responce.json();
+        })
+        .then(country => {
+            //if (country.length > 10) {
+            //    const myError = error({
+            //        text: "I'm an error message."
+            //    });
+            //    myError;
+            //        
+            //}
+            if (country.length === 1) {
+                refs.countryList.insertAdjacentHTML('beforeend', createCountryMarkup(country));
+                return
+            };
+            if (1 < country.length <= 10) {
+                refs.countryList.insertAdjacentHTML('beforeend', createCountryListMarkup(country));
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+refs.input.addEventListener('input', add);
